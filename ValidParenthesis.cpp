6b764ed751e7a9b2dab54f1358c98ca6a1d6b0c4
Raj Agrahari <81769395+RajAgrahari@users.noreply.github.com>
2023-04-11 18:53:05 +0530

    Easy

---
+++ ValidParenthesis.cpp
@@ -0,0 +1,24 @@
+//https://leetcode.com/problems/valid-parentheses/
+bool isValid(string s) {
+        int n=s.size();
+        stack<char> st;
+        for(int i=0;i<n;i++)
+        {
+            if(s[i]=='(' || s[i]=='[' || s[i]=='{')
+                st.push(s[i]);
+            else{
+                if(!st.empty() && s[i]==')' && st.top()=='(')
+                    st.pop();
+                else if(!st.empty() && s[i]=='}' && st.top()=='{')
+                    st.pop();
+                else if(!st.empty() && s[i]==']' && st.top()=='[')
+                    st.pop();
+                else{
+                    return false;
+                }
+            }
+        }
+        if(!st.empty())
+            return false;
+        return true;
+    }
