628b46b6476e3ca1bd572c319264afd0fb10c5c0
Raj Agrahari <81769395+RajAgrahari@users.noreply.github.com>
2023-05-18 23:39:07 +0530

    Medium

---
+++ monotonicStack/SumOfSubarraysMinimum.cpp
@@ -0,0 +1,24 @@
+//https://leetcode.com/problems/sum-of-subarray-minimums/description/
+ long long mod = 1000000007;
+    int sumSubarrayMins(vector<int>& arr) 
+    {
+            stack<int> st;
+            int ans = 0;
+
+            int n = arr.size();
+            for (int i = 0; i <= arr.size(); i++)
+            {
+                while (!st.empty() && (i == n || arr[st.top()] >= arr[i]))
+                {
+                    int idx = st.top();
+                    st.pop();
+
+                    long right = i < n ? i - idx : n - idx;
+                    long left = st.empty() ? idx + 1 : idx - st.top();
+
+                    ans = (ans + left *right *arr[idx]) % mod;
+                }
+                st.push(i);
+            }
+        return ans;
+    }
